4964bbe65917238da09424417e63551b21c81210
Arnaud Steiner <arnaudsteiner@orange.fr>
2023-05-03 18:25:20 +0200

    feat: User controller and reset-password API route added

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api/users')]
+class SecurityController extends AbstractController
+{
+    private $userPasswordHasher;
+    
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    #[Route('/{id}/reset-password', name: 'app_users_update_password', methods: ['PUT'])]
+    public function updatePassword(User $user, Request $request, SerializerInterface $serializer, EntityManagerInterface $manager): JsonResponse
+    {
+        $data = $request->getContent();
+        $data = json_decode($data, true);
+
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $data['password']));
+
+        $manager->persist($user);
+
+        $manager->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json');
+
+        return new JsonResponse($jsonUser, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+}
